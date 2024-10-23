5636508661cb7ff77a6252a26c2d875bece09904
Emma Gale <emmajgale@hotmail.com>
2024-10-23 19:02:15 +0100

    returns number if occurs more than once

--- odd-int.test.js
+++ odd-int.test.js
@@ -2,8 +2,11 @@ const oddInt = require('./oddInt');
 
 describe(oddInt, () => {
 	test('should return the number if it occurs once', () => {
+		expect(oddInt([1, 2, 1])).toBe(2);
 		expect(oddInt([7])).toBe(7);
 		expect(oddInt([0])).toBe(0);
-		expect(oddInt([1,1,2])).toBe(2);
-	});	
+		expect(oddInt([1, 1, 2])).toBe(2);
+	});
+
+	// test('should return 3 if it occurs 3 times'), () => {};
 });
--- oddInt.js
+++ oddInt.js
@@ -1,5 +1,17 @@
 function oddInt(nums) {
-  return nums[0];
+	let countmap = {};
+	nums.forEach((num) => {
+		if (countmap[num]) {
+			countmap[num]++;
+		} else countmap[num] = 1;
+	});
+
+	console.log(countmap);
+	for (const key in countmap) {
+		if (countmap[key] % 2 !== 0) {
+			return +key;
+		}
+	}
 }
 
 module.exports = oddInt;
